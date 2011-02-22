6722f500c1968d9cc0d310bde52005f5329d617b
arzynik <github@arzynik.com>
2011-02-22 13:42:17 -0800

    added basicl layout

---
+++ www/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Crystal Murray</title>
+<link rel="shortcut icon" href="/favicon.ico">
+<meta name="ROBOTS" content="INDEX,FOLLOW">
+<meta name="author" content="Devin Smith">
+<meta name="keywords" content="crystal, murray, public, relations, specialist, resume, marketing, atlus, naruto, deamons souls, video, game">
+<meta name="description" content="Why, hello there! IÕm a 26 year old Public Relations Specialist living in Los Angeles California.">
+<meta name="editor" content="Devin Smith">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<link rel="stylesheet" type="text/css" href="/assets/css/crystal.css">
+
+<body>
+
+<div class="container">
+	<div style="width: 600px; height: 1%;">
+		<div class="nav">
+			<div class="cloud-large"></div>
+			<div class="cloud-small cloud-small-resume">
+				<h3><a href="/Crystal-Murray_Resume_<?=date('Y-m-d')?>.doc">Resume</a></h3>
+			</div>
+			<div class="cloud-small cloud-small-linkedin">
+				<h3><a href="http://www.linkedin.com/in/crystalmurray" target="_blank">LinkedIn</a></h3>
+			</div>
+		<div style="clear: both;"></div>
+		</div>
+
+	</div>
+</div>
+<div class="content top">
+	<div class="container">
+		<div class="crystal"></div>
+		<div class="welcome">
+			<h1>Why, hello there!</h1>
+			<h2>I'm a 26 year old Public Relations Specialist living in Los Angeles, California.</h2>
+		</div>
+	</div>
+	<div class="content middle"></div>
+</div>
+<div class="content bottom">
+	<div class="container">
+	
+	</div>
+</div>
+</body>
+</html>
